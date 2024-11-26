170ce8f17bb5f7e4cdd039dc8ed92cb61fb0067d
MrYonatan <yonatanassefa60@gmail.com>
2024-11-26 19:24:46 +0300

    permission fixed

--- MylibApiProj/settings.py
+++ MylibApiProj/settings.py
@@ -106,6 +106,15 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- books/permissions.py
+++ books/permissions.py
@@ -4,9 +4,12 @@ from django.contrib.auth import get_user_model
 class IsAdminUserForCreate(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
-            return request.user.is_authenticated
+            return True
         return request.user and request.user.is_staff
     
 
-    
-#no has_object_permission class needed since we got no need for accessing the creator of the obj 
+# class ReadOnlyOrAuthenticatedWrite(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         if request.method in permissions.SAFE_METHODS:
+#             return True  # SAFE methods (GET, HEAD, OPTIONS) are allowed for everyone
+#         return request.user and request.user.is_authenticated
