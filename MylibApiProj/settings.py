be566fbdec73a410789ff30b786c5f07c7480c85
MrYonatan <yonatanassefa60@gmail.com>
2025-03-05 14:53:36 +0300

    superuser fix

--- MylibApiProj/settings.py
+++ MylibApiProj/settings.py
@@ -106,9 +106,6 @@ WSGI_APPLICATION = "MylibApiProj.wsgi.application"
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
